2f20245918cd20aeb2e3154cf6c438caf30f1d2a
Zaitam <gotzmatiascode@gmail.com>
2022-12-04 15:10:51 -0300

    Add script

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NAME="${1:-widget}"
+BRANCH="main"
+REPO="https://github.com/jiwe-app/widget-template.git"
+
+# Clone repo
+git clone -b $BRANCH $REPO $NAME
+cd $NAME
+
+# Replace placeholders
+F="WIDGET_NAME"
+find ./  -type f -name "*.swift" | xargs sed -i -e 's/$F/$NAME/g'
+echo $PWD
+
+# Restart git
+rm -rf .git
+git init
+git add .
+git commit -m "Init $NAME widget!"
